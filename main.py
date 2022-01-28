c0f4c2885ab23e9497788465f6c05c4548bb347e
Tolek Rudnicki <tolek@epsi.pl>
2022-01-28 18:38:16 +0100

    Diffrent input elements for win and linux

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import tparser
 import getpass
 import platform
 import os
+import sys
 from libs.colors import *
-
+import win32.main as win
 
 while (True):
     path = os.getcwd()
     home = os.path.expanduser('~')
-    if path.startswith(home):
-        path = path.replace(home, '~', 1)
-    cmd = input(Colors.LIGHT_GREEN + getpass.getuser() + "@" + platform.node() + Colors.WHITE + ":" + Colors.LIGHT_BLUE + path + Colors.WHITE + "$ ")
-    tparser.parse(cmd)
+    if sys.platform == "win32":
+        cmd = input(path + ">")
+        win.parse(cmd)
+    else:
+        if path.startswith(home):
+            path = path.replace(home, '~', 1)
+        cmd = input(Colors.LIGHT_GREEN + getpass.getuser() + "@" + platform.node() + Colors.WHITE + ":" + Colors.LIGHT_BLUE + path + Colors.WHITE + "$ ")
+        tparser.parse(cmd)
---
+++ win32/main.py
@@ -0,0 +1,4 @@
+def parse(command):
+    args = command.split(" ")
+    cmd = args[0]
+    print(command)
